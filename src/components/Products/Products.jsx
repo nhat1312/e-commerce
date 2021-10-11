bc792d59f240ad8c27690318adef60f7c6747cd5
nhat1312 <58421579+nhat1312@users.noreply.github.com>
2021-10-11 14:00:08 +0700

    fix

--- src/components/Products/Products.jsx
+++ src/components/Products/Products.jsx
@@ -4,7 +4,7 @@ import Grid from '@material-ui/core/Grid';
 import Product from './Product/Product';
 import useStyles from './styles';
 
-const Products = ({ products}) => {
+const Products = () => {
     const classes = useStyles();
 
     return (
@@ -12,13 +12,13 @@ const Products = ({ products}) => {
             <div className={classes.toolbar} />
             <Grid container justify="center" spacing={4}>
                 {products.map((product) => (
-                <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
-                <Product product={product} />
-            </Grid>
-        ))}
+                    <Grid key={product.id} item xs={12} sm={6} md={4} lg={3}>
+                        <Product product={product} />
+                    </Grid>
+                ))}
             </Grid>
         </main>
-  );
+    );
 }
 
 export default Products;
