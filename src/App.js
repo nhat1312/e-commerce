15dc197e67b3f6f2b6d011878f2f224a1fdfcdf6
nhat1312 <nhatlmgcs18207@fpt.edu.vn>
2021-11-30 01:38:19 +0700

    loginform
    Adding login function

--- src/App.js
+++ src/App.js
@@ -1,95 +1,119 @@
 import React, { useState, useEffect } from 'react';
-import { commerce } from './lib/commerce';
-import { Products, Navbar, Cart, Checkout } from './components'
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import { CssBaseline } from '@material-ui/core';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import axios from 'axios';
+import { Navbar, Products, Cart, Checkout, Login } from './components';
+import { commerce } from './lib/commerce';
+import { getToken, removeUserSession, setUserSession } from './utils/Common';
+
+import PublicRoute from './utils/PublicRoute';
 
 const App = () => {
-    const [mobileOpen, setMobileOpen] = React.useState(false);
-    const [products, setProducts] = useState([]);
-    const [cart, setCart] = useState({});
-    const [order, setOrder] = useState({});
-    const [errorMessage, setErrorMessage] = useState('');
+  const [mobileOpen, setMobileOpen] = React.useState(false);
+  const [products, setProducts] = useState([]);
+  const [cart, setCart] = useState({});
+  const [order, setOrder] = useState({});
+  const [errorMessage, setErrorMessage] = useState('');
+  const [authLoading, setAuthLoading] = useState(true);
 
-    const fetchProducts = async () => {
-        const { data } = await commerce.products.list();
+  const fetchProducts = async () => {
+    const { data } = await commerce.products.list();
 
-        setProducts(data);
+    setProducts(data);
+  };
+
+  const fetchCart = async () => {
+    setCart(await commerce.cart.retrieve());
+  };
+
+  const handleAddToCart = async (productId, quantity) => {
+    const item = await commerce.cart.add(productId, quantity);
+
+    setCart(item.cart);
+  };
+
+  const handleUpdateCartQty = async (lineItemId, quantity) => {
+    const response = await commerce.cart.update(lineItemId, { quantity });
+
+    setCart(response.cart);
+  };
+
+  const handleRemoveFromCart = async (lineItemId) => {
+    const response = await commerce.cart.remove(lineItemId);
+
+    setCart(response.cart);
+  };
+
+  const handleEmptyCart = async () => {
+    const response = await commerce.cart.empty();
+
+    setCart(response.cart);
+  };
+
+  const refreshCart = async () => {
+    const newCart = await commerce.cart.refresh();
+
+    setCart(newCart);
+  };
+
+  const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
+    try {
+      const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
+
+      setOrder(incomingOrder);
+
+      refreshCart();
+    } catch (error) {
+      setErrorMessage(error.data.error.message);
+    }
+  };
+
+  useEffect(() => {
+    fetchProducts();
+    fetchCart();
+    
+    const token = getToken();
+    if (!token) {
+        return;
     }
 
-    const fetchCart = async () => {
-        setCart(await commerce.cart.retrieve());
-    }
+  axios.get(`http://localhost:4000/verifyToken?token=${token}`).then(response => {
+    setUserSession(response.data.token, response.data.user);
+    setAuthLoading(false);
+}).catch(error => {
+    removeUserSession();
+    setAuthLoading(false);
+});
+}, []);
 
-    const handleAddToCart = async (productId, quantity) => {
-        const item = await commerce.cart.add(productId, quantity);
+if (authLoading && getToken()) {
+  return <div className="content">Checking Authentication...</div>
+}
 
-        setCart(item.cart);
-    }
+  const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
 
-    const handleUpdateCartQty = async (lineItemId, quantity) => {
-        const response = await commerce.cart.update(lineItemId, { quantity });
-
-        setCart(response.cart)
-    }
-
-    const handleRemoveFromCart = async (productId) => {
-        const response = await commerce.cart.remove(productId);
-
-        setCart(response.cart);
-    }
-
-    const handleEmptyCart = async () => {
-        const response = await commerce.cart.empty();
-
-        setCart(response.cart);
-    }
-
-    const refreshCart = async () => {
-        const newCart = await commerce.cart.refresh();
-
-        setCart(newCart);
-    }
-
-    const handleCaptureCheckout = async (checkoutTokenId, newOrder) => {
-        try {
-            const incomingOrder = await commerce.checkout.capture(checkoutTokenId, newOrder);
-
-            setOrder(incomingOrder);
-
-            refreshCart();
-        } catch (error) {
-            setErrorMessage(error.date.error.message);
-        }
-    }
-
-    useEffect(() => {
-        fetchProducts();
-        fetchCart();
-    }, []);
-
-    console.log(cart);
-    console.log(products);
-
-    const handleDrawerToggle = () => setMobileOpen(!mobileOpen);
-    return (
-        <Router>
-            <div style={{ display: 'flex' }}>
-                <CssBaseline />
-                <Navbar totalItems={cart.total_items} handleDrawerToggle={handleDrawerToggle} />
-                <Switch>
-                    <Route exact path="/">
-                        <Products products={products} onAddToCart={handleAddToCart} handleUpdateCartQty />
-                    </Route>
-                    <Route exact path="/cart">
-                        <Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart} />
-                    </Route>
-                    <Route exact path="/checkout">
-                        <Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />
-                    </Route>
-                </Switch>
-            </div>
-        </Router>
-    );
+  return (
+    <Router>
+      <div style={{ display: 'flex' }}>
+        <CssBaseline />
+        <Navbar totalItems={cart.total_items} handleDrawerToggle={handleDrawerToggle} />
+        <Switch>
+          <Route exact path="/">
+            <Products products={products} onAddToCart={handleAddToCart} handleUpdateCartQty />
+          </Route>
+          <Route exact path="/cart">
+            <Cart cart={cart} onUpdateCartQty={handleUpdateCartQty} onRemoveFromCart={handleRemoveFromCart} onEmptyCart={handleEmptyCart} />
+          </Route>
+          <Route path="/checkout" exact>
+            <Checkout cart={cart} order={order} onCaptureCheckout={handleCaptureCheckout} error={errorMessage} />
+          </Route>
+          <PublicRoute exact path="/login">
+                        <Login />
+                    </PublicRoute>
+        </Switch>
+      </div>
+    </Router>
+  );
 };
+
 export default App;
